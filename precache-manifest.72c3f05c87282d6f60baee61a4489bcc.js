88d7b75f44be96e1091fc6882c041a125b3ac267
Roman Parajuli <notorious.garage@gmail.com>
2020-09-02 15:47:15 +0545

    Updates

---
+++ precache-manifest.72c3f05c87282d6f60baee61a4489bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d997d4475a27124ae683595429ed6209",
+    "url": "/RomanEmperor/petVet/index.html"
+  },
+  {
+    "revision": "ecdebe0a92ef7ca1ef9d",
+    "url": "/RomanEmperor/petVet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc371c24266534fbe773",
+    "url": "/RomanEmperor/petVet/static/js/2.329c4273.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RomanEmperor/petVet/static/js/2.329c4273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdebe0a92ef7ca1ef9d",
+    "url": "/RomanEmperor/petVet/static/js/main.96ec279a.chunk.js"
+  },
+  {
+    "revision": "136c0624e32a5fcdd263",
+    "url": "/RomanEmperor/petVet/static/js/runtime-main.d2076f7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RomanEmperor/petVet/static/media/logo.5d5d9eef.svg"
+  }
+]);
